5843fa51598bca0259d7a9ff5ac0854a9624c9dc
madilynsimons <madilynsimons@gmail.com>
2017-03-23 13:58:54 -0400

    insertion sortgit add InsertionSortTest.class

--- InsertionSortTest.java
+++ InsertionSortTest.java
@@ -7,22 +7,34 @@
 import java.util.*;
 
 public class InsertionSortTest{
+  
+  public static int icount = 0;
 
   public static void main(String[] args){   
-    Random gen=new Random(); int[] a=new int[15];
+    Random gen=new Random(); int[] a=new int[32];
     int i; for (i=0; i<a.length; i++) a[i]=gen.nextInt(5000)+1;
     System.out.println("Initial array:");
     for (i=0; i<a.length; i++) System.out.println(a[i] + " ");
     System.out.println();
-    insertionsort();
+    insertionsort(a);
     System.out.println("Sorted array:");
     for (i=0; i<a.length; i++) System.out.println(a[i] + " ");
     System.out.println("Done!");
-    System.out.println(qcount+" qcount");
+    System.out.println(icount+" icount");
   }
   
-  public static void insertionsort(){
-    
+  public static void insertionsort(int[] data){
+    for(int x = 0; x < data.length-1; x++){
+      int j = x;
+      int insert = x+1;
+      while(j > -1 && data[insert] < data[j]){
+        icount++;
+        int temp = data[j];
+        data[j] = data[insert];
+        data[insert] = temp;
+        j--; insert--;
+      }
+    }
   }
   
 }

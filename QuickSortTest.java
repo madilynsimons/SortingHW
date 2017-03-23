44ce17d1ee7c9ed03a933a39d6980e16408277d5
madilynsimons <madilynsimons@gmail.com>
2017-03-23 12:58:19 -0400

    fourth commit

--- QuickSortTest.java
+++ QuickSortTest.java
@@ -5,7 +5,7 @@ public class QuickSortTest{
   static int qcount = 0;
 
   public static void main(String[] args){
-    Random gen=new Random(); int[] a=new int[10];
+    Random gen=new Random(); int[] a=new int[15];
     int i; for (i=0; i<a.length; i++) a[i]=gen.nextInt(5000)+1;
     System.out.println("Initial array:");
     for (i=0; i<a.length; i++) System.out.println(a[i] + " ");
@@ -18,21 +18,22 @@ public class QuickSortTest{
   }
 
   public static void quicksort (int[] data, int i, int j){
+
     int p; int low = i; int high = j;
-    p = (high-low)/2 + low;
+    p = low + (high-low)/2;
     while(low < high){
       while(data[low] < data[p]) low++;
-      while(data[high]>data[p]) high--;
+      while(data[high] > data[p]) high--;
       if(low<high){
         qcount++;
         int temp = data[high];
         data[high] = data[low];
         data[low] = temp;
-        low++; high--;
-      }
+      }low++; high--;
     }
     if(low<j) quicksort (data,i,p);
-    if(i<high) quicksort (data,p,j);
+    if(i<high) quicksort (data,p+1, j);
+    
   }
-
+  
 }
